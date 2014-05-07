28b8aab6b96688da7ca2d6e032c1e1420f1c091f
Naveen Manivannan <naveen.manivannan@gmail.com>
2014-05-06 23:50:33 -0400

    move everything to /tmp

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 make:
 	@echo "Please run ./import_data.sh [MYSQL_USER] [MYSQL_PASSWORD] [MYSQL_DB]"
 clean:
-	rm -rf tmp/*
+	rm -rf /tmp/usda_tmp
--- import_data.sh
+++ import_data.sh
@@ -19,38 +19,39 @@ export MY_USER=$2
 export MY_PASS=$3
 export MY_DB=$4
 
-export MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export MY_DIR=/tmp
 
 # Current release
 export DL_URL=https://www.ars.usda.gov/SP2UserFiles/Place/12354500/Data/SR26/dnload/sr26.zip
 export DL_URL2=https://www.ars.usda.gov/SP2UserFiles/Place/12354500/Data/SR26/dnload/sr26abbr.zip
 
-if [ ! -d "$MY_DIR/tmp" ]; then
+if [ ! -d "$MY_DIR/usda_tmp" ]; then
 	echo 'Local tmp directory not found... creating one'
-	mkdir -p $MY_DIR/tmp
+	mkdir -p $MY_DIR/usda_tmp
 fi
 
 echo 'Clearing all data in tmp directory'
-rm -rf $MY_DIR/tmp/*
+rm -rf $MY_DIR/usda_tmp/*
 
-wget $DL_URL -P $MY_DIR/tmp
-wget $DL_URL2 -P $MY_DIR/tmp
-ls tmp/*.zip | xargs -n1 unzip -o -d tmp/
+wget $DL_URL -P $MY_DIR/usda_tmp
+wget $DL_URL2 -P $MY_DIR/usda_tmp
+ls $MY_DIR/usda_tmp/*.zip | xargs -n1 unzip -o -d $MY_DIR/usda_tmp/
 
-dos2unix tmp/*.txt
+dos2unix $MY_DIR/usda_tmp/*.txt
 echo 'Generating raw data SQL statements'
-ls tmp/*.txt | xargs -n1 basename | perl -ne 'chomp; s/\.txt$//i;
+ls $MY_DIR/usda_tmp/*.txt | xargs -n1 basename | perl -ne 'chomp; s/\.txt$//i;
 print <<"EOF"
-LOAD DATA LOCAL INFILE "$ENV{MY_DIR}/tmp/$_.txt"
+LOAD DATA LOCAL INFILE "$ENV{MY_DIR}/usda_tmp/$_.txt"
 INTO TABLE $_ FIELDS TERMINATED BY "\^"
 ENCLOSED BY "\~"
 LINES TERMINATED BY "\\n";
 
 EOF
-' > $MY_DIR/tmp/raw_data.sql
+' > $MY_DIR/usda_tmp/raw_data.sql
 
 echo 'Executing combined raw and schema SQL statements...'
-cat $MY_DIR/schema.sql $MY_DIR/tmp/raw_data.sql > $MY_DIR/tmp/fin_out.sql
-mysql --local-infile=1 -h$MY_HOSTNAME -u$MY_USER -p$MY_PASS $MY_DB < $MY_DIR/tmp/fin_out.sql
+cat $CURR_DIR/schema.sql $MY_DIR/usda_tmp/raw_data.sql > $MY_DIR/usda_tmp/fin_out.sql
+mysql --local-infile=1 -h$MY_HOSTNAME -u$MY_USER -p$MY_PASS $MY_DB < $MY_DIR/usda_tmp/fin_out.sql
 
 echo 'DONE'
