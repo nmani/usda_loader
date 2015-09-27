d1818fe73463030370b7883599f35a19ccbd4f98
Naveen Manivannan <naveen.manivannan@gmail.com>
2015-09-27 14:18:52 -0400

    output

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM mysql:latest
+MAINTAINER Naveen Manivannan <naveen{dot}manivannan(at)gmail{dot}com>
+
+WORKDIR /tmp
+
+RUN apt-get update && apt-get -y upgrade && apt-get install -y \
+     wget dos2unix unzip ca-certificates
+
+ADD schema.sql /tmp/
+ADD import_data.sh /tmp/
+
--- import_data.sh
+++ import_data.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 3 ]; then
-	echo "Expecting: './import_data.sh [HOSTNAME] [MYSQL_USER] [MYSQL_PASSWORD] [MYSQL_DB]'"
-	exit
-fi
+#if [ $# -lt 3 ]; then
+#	echo "[ERROR] Expecting: './import_data.sh [HOSTNAME] [MYSQL_USER] [MYSQL_PASSWORD] [MYSQL_DB]'"
+#	exit
+#fi
 
 # Check for shit we need
-echo 'Checking for necessary programs...'
+echo '[INFO] Checking for necessary programs...'
 UTILS=("dos2unix" "wget" "mysql" "unzip" "command")
 for util in "${UTILS[@]}"
 do
-	command -v $util >/dev/null 2>&1 || { echo >&2 "Please install $util"; exit 1; }
-	echo "Found: $util"
+	command -v $util >/dev/null 2>&1 || { echo >&2 "[ERROR] Please install $util"; exit 1; }
+	echo "[INFO] Found: $util"
 done
 
 export MY_HOSTNAME=$1
-export MY_USER=$2
-export MY_PASS=$3
-export MY_DB=$4
+export MY_USER=$MYSQL_USER
+export MY_PASS=$MYSQL_PASSWORD
+export MY_DB=$MYSQL_DATABASE
 
 export CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export MY_DIR=/tmp
@@ -27,11 +27,11 @@ export DL_URL=https://www.ars.usda.gov/SP2UserFiles/Place/12354500/Data/SR26/dnl
 export DL_URL2=https://www.ars.usda.gov/SP2UserFiles/Place/12354500/Data/SR26/dnload/sr26abbr.zip
 
 if [ ! -d "$MY_DIR/usda_tmp" ]; then
-	echo 'Local tmp directory not found... creating one'
+	echo '[INFO] Local tmp directory not found... creating one'
 	mkdir -p $MY_DIR/usda_tmp
 fi
 
-echo 'Clearing all data in tmp directory'
+echo '[INFO] Clearing all data in tmp directory'
 rm -rf $MY_DIR/usda_tmp/*
 
 wget $DL_URL -P $MY_DIR/usda_tmp
@@ -39,7 +39,7 @@ wget $DL_URL2 -P $MY_DIR/usda_tmp
 ls $MY_DIR/usda_tmp/*.zip | xargs -n1 unzip -o -d $MY_DIR/usda_tmp/
 
 dos2unix $MY_DIR/usda_tmp/*.txt
-echo 'Generating raw data SQL statements'
+echo '[INFO] Generating raw data SQL statements'
 ls $MY_DIR/usda_tmp/*.txt | xargs -n1 basename | perl -ne 'chomp; s/\.txt$//i;
 print <<"EOF"
 LOAD DATA LOCAL INFILE "$ENV{MY_DIR}/usda_tmp/$_.txt"
@@ -50,8 +50,10 @@ LINES TERMINATED BY "\\n";
 EOF
 ' > $MY_DIR/usda_tmp/raw_data.sql
 
-echo 'Executing combined raw and schema SQL statements...'
+echo '[INFO] Executing combined raw and schema SQL statements...'
 cat $CURR_DIR/schema.sql $MY_DIR/usda_tmp/raw_data.sql > $MY_DIR/usda_tmp/fin_out.sql
 mysql --local-infile=1 -h$MY_HOSTNAME -u$MY_USER -p$MY_PASS $MY_DB < $MY_DIR/usda_tmp/fin_out.sql
 
-echo 'DONE'
+echo '[INFO] Dump the .sql file for output'
+mysqldump -h$MY_HOSTNAME -u$MY_USER -p$MY_PASS $MY_DB > $MY_DIR/usda_tmp/db_dump.sql
+echo '[INFO] DONE'
